9be1a25293e099400117cd32da8641e4958b9460
work-adityakr <work.adityakr@gmail.com>
2026-02-17 14:07:44 +0530

    he

--- config/postgresql.js
+++ config/postgresql.js
@@ -1,10 +1,30 @@
+// import "dotenv/config";
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import { PrismaClient } from '@prisma/client'
+
+// const connectionString = `${process.env.DATABASE_URL}`
+
+// const adapter = new PrismaPg({ connectionString })
+// const prisma = new PrismaClient({ adapter })
+
+// export { prisma }
+
+
+
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
+import pkg from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const { PrismaClient } = pkg;
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing");
+}
 
-export { prisma }
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
